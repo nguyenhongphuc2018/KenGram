a49cc82d864f2d7a1cb73891a466cf578ad87cc5
Phuong Pham <phuongptt.uet@gmail.com>
2018-07-01 10:05:44 +0700

    new feed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,10 @@
+//= require_self
+//= require myjs
 //= require jquery
 //= require bootstrap
 //= require jquery
 //= require bootstrap
 //= require rails-ujs
 //= require turbolinks
+//= require jquery.flexslider
 //= require_tree .
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,6 @@ class PostsController < ApplicationController
   before_action :load_post, only: %i(show edit update destroy)
   before_action :logged_in_user, only: %i(create destroy)
 
-  def index;  end
-
   def new
     @post = Post.new
     @post_attachment = @post.post_attachments.build
@@ -22,13 +20,18 @@ class PostsController < ApplicationController
     end
   end
 
-  def show;  end
+  def show
+    @post_attachments = @post.post_attachments.all
+  end
 
-  def edit;  end
-
-  def update;  end
-
-  def destroy;  end
+  def destroy;
+    if @post.destroy
+      flash[:success] = t ".flash_sucess_destroy"
+    else
+      flash[:danger] = t ".flash_danger_destroy"
+    end
+    redirect_to request.referrer || root_url
+  end
 
   private
     def load_post
